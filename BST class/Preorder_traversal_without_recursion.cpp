64d677f6ee2569a33fd6c31cb478b2c5b0801f9a
Khushi Chavada <khushichavada0112@gmail.com>
2020-06-29 13:35:34 +0530

    Add files via upload

---
+++ BST class/Preorder_traversal_without_recursion.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<stack>
+#include<cstdlib>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+}*root=NULL;
+
+
+node* insertion (node* root,int n)
+{
+    if (root==NULL)
+    {
+        root=new node;
+        root->data=n;
+        root->left=root->right=NULL;
+        return root;
+    }
+
+    else
+   {
+        if (n<root->data)
+            root->left=insertion(root->left,n);
+        else
+            root->right=insertion (root->right,n);
+        return (root);
+    }
+}
+
+void display(node* root)
+{
+    stack<node*>s;
+    s.push(root);
+    while(!s.empty())
+    {
+        node* temp=s.top();
+        s.pop();
+        cout<<temp->data<<" ";
+        if(temp->right!=nullptr)
+           s.push(temp->right);
+        if(temp->left!=nullptr)
+           s.push(temp->left);
+    }
+}
+
+int main(){
+
+    root=insertion(root,40);
+    root=insertion(root,50);
+    root=insertion(root,10);
+    root=insertion(root,30);
+    root=insertion(root,70);
+    root=insertion(root,90);
+    root=insertion(root,20);
+    root=insertion(root,35);
+    display(root);
+}
